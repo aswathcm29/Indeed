44008462f5687cfec6a7b4f0a2157801c0c04c98
Cibiyanna <pkcibiyanna@gmail.com>
2024-04-02 21:51:53 +0530

    modify if else for line reducing

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -5,34 +5,31 @@ const bcrypt = require('bcrypt');
 
 
 const login = async(req,res)=>{
-    console.log('login check')
     try{
-    const username = req.body.username;
-    const password = req.body.password;
-    console.log(username,password)
-    if(username!=='' && password!==''){
+        const username = req.body.username;
+        const password = req.body.password;
+
+        // always handle false case first to avoid nested if else
+        if(!username || !password) {
+            return res.status(500).json({ error: true, message: 'Username or password cannot be empty' })
+        }
+
         const user = await userModel.findOne({username:username});
         if(!user){
             return res.status(500).json({error:true,message:'User not found'})
         }
-    
-        const passwordMatch = await bcrypt.compare(password,user.password);
-        
-        if(passwordMatch){
-            const authToken = await generateToken(username,"user");
-            if(authToken===""){
-                return res.status(500).json({error:true,message:'Token generation failed'})
-            }
-            res.cookie('token',authToken);
-            return res.status(200).json({error:false,message:{token:authToken,userType:'user'}})
+
+        const passwordMatch =  bcrypt.compare(password,user.password);  // bcrypt don't need await
+        if(!passwordMatch) {
+            return res.status(500).json({ error: true, message: 'Password does not match' })
+        }  
+            
+        const authToken = await generateToken(username,"user");
+        if(authToken===""){
+            return res.status(500).json({error:true,message:'Token generation failed'})
         }
-        else{
-            return res.status(500).json({error:true,message:'Password does not match'})
-        }
-    }
-    else{
-        return res.status(500).json({error:true,message:'Username or password cannot be empty'})
-    }
+        res.cookie('token',authToken);
+        return res.status(200).json({error:false,message:{token:authToken,userType:'user'}})
     }
     catch(err){
         return res.status(404).json({error:true,message:err.message})
@@ -40,6 +37,7 @@ const login = async(req,res)=>{
 }
 
 const signup = async(req,res)=>{
+
     const username  = req.body.username;
     const password = req.body.password;
     const email = req.body.email;
@@ -47,9 +45,11 @@ const signup = async(req,res)=>{
     if(username=='' || password==''||email==''){
         return res.status(401).json({error:true,message:'Invalid credentials'})
     }
+
     if(username == undefined || password == undefined || email == undefined){
         return res.status(401).json({error: true, message:"invalid credentials"})
     }
+
     try{
         const response = await userModel.findOne({username:username});
         if(response){
@@ -67,10 +67,10 @@ const signup = async(req,res)=>{
                 return res.status(500).json({error:true,message:err.message})
             }
     }
-        catch(err){
-            return res.status(500).json({error:true,message:err.message})
-        }
 
+    catch(err){
+        return res.status(500).json({error:true,message:err.message})
+    }
 }
 
 module.exports = {signup,login}
